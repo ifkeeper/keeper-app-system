9aea4fda44f9ffe42b0b07edd0c247b5c3da0f25
MinGRn <MinGRn97@gmail.com>
2019-02-23 23:39:32 +0800

    增加 Feign 客户端;

--- src/main/java/com/mingrn/keeper/system/SystemApplication.java
+++ src/main/java/com/mingrn/keeper/system/SystemApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @ComponentScan(basePackages = "com.mingrn.keeper")
+@EnableFeignClients(basePackages = "com.mingrn.keeper")
 public class SystemApplication {
 
 	public static void main(String[] args) {
