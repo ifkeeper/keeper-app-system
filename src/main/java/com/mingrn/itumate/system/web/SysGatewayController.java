e5f33b423caaa99cbecd66b108ba9c725d869e43
MinGRn <MinGRn97@gmail.com>
2019-07-07 15:19:57 +0800

    方法名修改

--- src/main/java/com/mingrn/itumate/system/web/SysGatewayController.java
+++ src/main/java/com/mingrn/itumate/system/web/SysGatewayController.java
@@ -31,124 +31,124 @@ import java.util.Random;
 @Api(description = "System Gateway Operate")
 public class SysGatewayController {
 
-	@Resource
-	private SysGatewayService sysGatewayService;
+    @Resource
+    private SysGatewayService sysGatewayService;
 
-	@SuppressWarnings("unchecked")
-	@GetMapping("/findList")
-	@ApiOperation(value = "列表查询")
-	@ApiImplicitParams({
-			@ApiImplicitParam(paramType = "query", dataTypeClass = Integer.class, required = true, defaultValue = "1", name = "pageNumber", value = "页码"),
-			@ApiImplicitParam(paramType = "query", dataTypeClass = Integer.class, required = true, defaultValue = "10", name = "sizeSize", value = "每页数量")
-	})
-	public Result findList(@RequestParam(defaultValue = "1") Integer pageNumber,
-						   @RequestParam(defaultValue = "10") Integer sizeSize) {
+    @SuppressWarnings("unchecked")
+    @GetMapping("/findList")
+    @ApiOperation(value = "列表查询")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "query", dataTypeClass = Integer.class, required = true, defaultValue = "1", name = "pageNumber", value = "页码"),
+            @ApiImplicitParam(paramType = "query", dataTypeClass = Integer.class, required = true, defaultValue = "10", name = "sizeSize", value = "每页数量")
+    })
+    public Result findList(@RequestParam(defaultValue = "1") Integer pageNumber,
+                           @RequestParam(defaultValue = "10") Integer sizeSize) {
 
-		PageHelper.startPage(pageNumber, sizeSize);
-		List<SysGateway> sysGatewayList = sysGatewayService.find();
-		PageInfo pageInfo = new PageInfo(sysGatewayList);
+        PageHelper.startPage(pageNumber, sizeSize);
+        List<SysGateway> sysGatewayList = sysGatewayService.find();
+        PageInfo pageInfo = new PageInfo(sysGatewayList);
 
-		return ResponseMsgUtil.success(pageInfo);
-	}
+        return ResponseMsgUtil.success(pageInfo);
+    }
 
-	@Checked
-	@PutMapping("/putSysGateway")
-	@ApiOperation(value = "新增路由")
-	@ApiImplicitParams({
-			@ApiImplicitParam(paramType = "query", dataType = "String", required = true, name = "appName", value = "网关应用名称"),
-			@ApiImplicitParam(paramType = "query", dataType = "String", required = true, name = "path", value = "路由规则。如:/hello-api/**"),
-			@ApiImplicitParam(paramType = "query", dataType = "String", required = true, name = "serviceId", value = "服务id。如：hello-service。与 url 只能存在一个。"),
-			@ApiImplicitParam(paramType = "query", dataType = "String", required = false, name = "url", value = "服务地址。如：http://localhost:8080。与 服务ID 只能存在一个"),
-			@ApiImplicitParam(paramType = "query", dataType = "Boolean", required = true, name = "retryable", value = "是否开启重试机制"),
-			@ApiImplicitParam(paramType = "query", dataType = "Boolean", required = true, name = "stripPrefix", value = "是否剥离路由前缀，统一剥离"),
-			@ApiImplicitParam(paramType = "query", dataType = "Boolean", required = true, name = "customSensitiveHeaders", value = "是否开启敏感头信息"),
-			@ApiImplicitParam(paramType = "query", dataType = "Boolean", required = true, name = "applyEnable", value = "是否立即启用路由"),
-			@ApiImplicitParam(paramType = "query", dataType = "String", required = true, name = "status", value = "状态。1：启用，2：禁用"),
-			@ApiImplicitParam(paramType = "query", dataType = "String", required = true, name = "description", value = "说明")
+    @Checked
+    @PutMapping("/put")
+    @ApiOperation(value = "新增路由")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "query", dataType = "String", required = true, name = "appName", value = "网关应用名称"),
+            @ApiImplicitParam(paramType = "query", dataType = "String", required = true, name = "path", value = "路由规则。如:/hello-api/**"),
+            @ApiImplicitParam(paramType = "query", dataType = "String", required = true, name = "serviceId", value = "服务id。如：hello-service。与 url 只能存在一个。"),
+            @ApiImplicitParam(paramType = "query", dataType = "String", required = false, name = "url", value = "服务地址。如：http://localhost:8080。与 服务ID 只能存在一个"),
+            @ApiImplicitParam(paramType = "query", dataType = "Boolean", required = true, name = "retryable", value = "是否开启重试机制"),
+            @ApiImplicitParam(paramType = "query", dataType = "Boolean", required = true, name = "stripPrefix", value = "是否剥离路由前缀，统一剥离"),
+            @ApiImplicitParam(paramType = "query", dataType = "Boolean", required = true, name = "customSensitiveHeaders", value = "是否开启敏感头信息"),
+            @ApiImplicitParam(paramType = "query", dataType = "Boolean", required = true, name = "applyEnable", value = "是否立即启用路由"),
+            @ApiImplicitParam(paramType = "query", dataType = "String", required = true, name = "status", value = "状态。1：启用，2：禁用"),
+            @ApiImplicitParam(paramType = "query", dataType = "String", required = true, name = "description", value = "说明")
 
-	})
-	public Result putSysGateway(@ParamsIsNotNull @RequestParam String path,
-								@ParamsIsNotNull @RequestParam String appName,
-								@ParamsIsNotNull @RequestParam Integer status,
-								@ParamsIsNotNull @RequestParam String serviceId,
-								@ParamsIsNotNull @RequestParam Boolean retryable,
-								@ParamsIsNotNull @RequestParam String description,
-								@ParamsIsNotNull @RequestParam Boolean stripPrefix,
-								@ParamsIsNotNull @RequestParam Boolean applyEnable,
-								@ParamsIsNotNull @RequestParam Boolean customSensitiveHeaders,
-								@RequestParam(required = false) String url) {
+    })
+    public Result put(@ParamsIsNotNull @RequestParam String path,
+                      @ParamsIsNotNull @RequestParam String appName,
+                      @ParamsIsNotNull @RequestParam Integer status,
+                      @ParamsIsNotNull @RequestParam String serviceId,
+                      @ParamsIsNotNull @RequestParam Boolean retryable,
+                      @ParamsIsNotNull @RequestParam String description,
+                      @ParamsIsNotNull @RequestParam Boolean stripPrefix,
+                      @ParamsIsNotNull @RequestParam Boolean applyEnable,
+                      @ParamsIsNotNull @RequestParam Boolean customSensitiveHeaders,
+                      @RequestParam(required = false) String url) {
 
-		SysGateway sysGateway = new SysGateway();
-		sysGateway.setAppName(appName);
-		sysGateway.setPath(path);
-		sysGateway.setServiceId(serviceId.toUpperCase());
-		sysGateway.setUrl(url);
-		sysGateway.setRetryable(retryable);
-		sysGateway.setStripPrefix(stripPrefix);
-		sysGateway.setCustomSensitiveHeaders(customSensitiveHeaders);
-		sysGateway.setApplyEnable(applyEnable);
-		sysGateway.setStatus(status);
-		sysGateway.setCreateTime(new Date());
-		sysGateway.setDescription(description);
-		sysGateway.setDeleted(DeletedEnum.NORMAL.getCode());
+        SysGateway sysGateway = new SysGateway();
+        sysGateway.setAppName(appName);
+        sysGateway.setPath(path);
+        sysGateway.setServiceId(serviceId.toUpperCase());
+        sysGateway.setUrl(url);
+        sysGateway.setRetryable(retryable);
+        sysGateway.setStripPrefix(stripPrefix);
+        sysGateway.setCustomSensitiveHeaders(customSensitiveHeaders);
+        sysGateway.setApplyEnable(applyEnable);
+        sysGateway.setStatus(status);
+        sysGateway.setCreateTime(new Date());
+        sysGateway.setDescription(description);
+        sysGateway.setDeleted(DeletedEnum.NORMAL.getCode());
 
-		sysGatewayService.insert(sysGateway);
-		return ResponseMsgUtil.success();
-	}
-
-
-	@Checked
-	@DeleteMapping("/remove")
-	@ApiOperation(value = "主键删除", notes = "支持批量删除")
-	@ApiImplicitParam(paramType = "query", dataTypeClass = String.class, required = true, name = "ids", value = "主键：id 或 id1,id2...")
-	public Result remove(@ParamsIsNotNull String ids) {
-
-		sysGatewayService.removeByIds(ids);
-		return ResponseMsgUtil.success();
-	}
+        sysGatewayService.insert(sysGateway);
+        return ResponseMsgUtil.success();
+    }
 
 
-	@Checked
-	@PostMapping("/updateSysGateway")
-	@ApiOperation(value = "更新网关", notes = "通过主键更新网关")
-	@ApiImplicitParams({
-			@ApiImplicitParam(paramType = "query", dataType = "String", required = true, name = "id", value = "主键"),
-			@ApiImplicitParam(paramType = "query", dataType = "String", required = true, name = "appName", value = "网关应用名称"),
-			@ApiImplicitParam(paramType = "query", dataType = "String", required = true, name = "path", value = "路由规则。如:/hello-api/**"),
-			@ApiImplicitParam(paramType = "query", dataType = "String", required = true, name = "serviceId", value = "服务id。如：hello-service。与 url 只能存在一个。"),
-			@ApiImplicitParam(paramType = "query", dataType = "String", required = false, name = "url", value = "服务地址。如：http://localhost:8080。与 服务ID 只能存在一个"),
-			@ApiImplicitParam(paramType = "query", dataType = "Boolean", required = true, name = "retryable", value = "是否开启重试机制"),
-			@ApiImplicitParam(paramType = "query", dataType = "Boolean", required = true, name = "stripPrefix", value = "是否剥离路由前缀，统一剥离"),
-			@ApiImplicitParam(paramType = "query", dataType = "Boolean", required = true, name = "customSensitiveHeaders", value = "是否开启敏感头信息"),
-			@ApiImplicitParam(paramType = "query", dataType = "Boolean", required = true, name = "applyEnable", value = "是否立即启用路由"),
-			@ApiImplicitParam(paramType = "query", dataType = "String", required = true, name = "status", value = "状态。1：启用，2：禁用"),
-			@ApiImplicitParam(paramType = "query", dataType = "String", required = true, name = "description", value = "说明")
+    @Checked
+    @DeleteMapping("/remove")
+    @ApiOperation(value = "主键删除", notes = "支持批量删除")
+    @ApiImplicitParam(paramType = "query", dataTypeClass = String.class, required = true, name = "ids", value = "主键：id 或 id1,id2...")
+    public Result remove(@ParamsIsNotNull String ids) {
 
-	})
-	public Result updateSysGateway(@ParamsIsNotNull @RequestParam String id,
-								   @ParamsIsNotNull @RequestParam String path,
-								   @ParamsIsNotNull @RequestParam String appName,
-								   @ParamsIsNotNull @RequestParam String serviceId,
-								   @ParamsIsNotNull @RequestParam Boolean retryable,
-								   @ParamsIsNotNull @RequestParam Boolean applyEnable,
-								   @ParamsIsNotNull @RequestParam Boolean stripPrefix,
-								   @ParamsIsNotNull @RequestParam Boolean customSensitiveHeaders,
-								   @ParamsIsNotNull @RequestParam Integer status, @RequestParam(required = false) String url,
-								   @ParamsIsNotNull @RequestParam String description) throws NoSuchFieldException, IllegalAccessException {
+        sysGatewayService.removeByIds(ids);
+        return ResponseMsgUtil.success();
+    }
 
-		SysGateway sysGateway = new SysGateway();
-		sysGateway.setAppName(appName);
-		sysGateway.setPath(path);
-		sysGateway.setServiceId(serviceId.toUpperCase());
-		sysGateway.setUrl(url);
-		sysGateway.setRetryable(retryable);
-		sysGateway.setStripPrefix(stripPrefix);
-		sysGateway.setCustomSensitiveHeaders(customSensitiveHeaders);
-		sysGateway.setApplyEnable(applyEnable);
-		sysGateway.setStatus(status);
-		sysGateway.setDescription(description);
 
-		sysGatewayService.update(sysGateway, id);
-		return ResponseMsgUtil.success();
-	}
+    @Checked
+    @PostMapping("/post")
+    @ApiOperation(value = "更新网关", notes = "通过主键更新网关")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "query", dataType = "String", required = true, name = "id", value = "主键"),
+            @ApiImplicitParam(paramType = "query", dataType = "String", required = true, name = "appName", value = "网关应用名称"),
+            @ApiImplicitParam(paramType = "query", dataType = "String", required = true, name = "path", value = "路由规则。如:/hello-api/**"),
+            @ApiImplicitParam(paramType = "query", dataType = "String", required = true, name = "serviceId", value = "服务id。如：hello-service。与 url 只能存在一个。"),
+            @ApiImplicitParam(paramType = "query", dataType = "String", required = false, name = "url", value = "服务地址。如：http://localhost:8080。与 服务ID 只能存在一个"),
+            @ApiImplicitParam(paramType = "query", dataType = "Boolean", required = true, name = "retryable", value = "是否开启重试机制"),
+            @ApiImplicitParam(paramType = "query", dataType = "Boolean", required = true, name = "stripPrefix", value = "是否剥离路由前缀，统一剥离"),
+            @ApiImplicitParam(paramType = "query", dataType = "Boolean", required = true, name = "customSensitiveHeaders", value = "是否开启敏感头信息"),
+            @ApiImplicitParam(paramType = "query", dataType = "Boolean", required = true, name = "applyEnable", value = "是否立即启用路由"),
+            @ApiImplicitParam(paramType = "query", dataType = "String", required = true, name = "status", value = "状态。1：启用，2：禁用"),
+            @ApiImplicitParam(paramType = "query", dataType = "String", required = true, name = "description", value = "说明")
+
+    })
+    public Result post(@ParamsIsNotNull @RequestParam String id,
+                       @ParamsIsNotNull @RequestParam String path,
+                       @ParamsIsNotNull @RequestParam String appName,
+                       @ParamsIsNotNull @RequestParam String serviceId,
+                       @ParamsIsNotNull @RequestParam Boolean retryable,
+                       @ParamsIsNotNull @RequestParam Boolean applyEnable,
+                       @ParamsIsNotNull @RequestParam Boolean stripPrefix,
+                       @ParamsIsNotNull @RequestParam Boolean customSensitiveHeaders,
+                       @ParamsIsNotNull @RequestParam Integer status, @RequestParam(required = false) String url,
+                       @ParamsIsNotNull @RequestParam String description) throws NoSuchFieldException, IllegalAccessException {
+
+        SysGateway sysGateway = new SysGateway();
+        sysGateway.setAppName(appName);
+        sysGateway.setPath(path);
+        sysGateway.setServiceId(serviceId.toUpperCase());
+        sysGateway.setUrl(url);
+        sysGateway.setRetryable(retryable);
+        sysGateway.setStripPrefix(stripPrefix);
+        sysGateway.setCustomSensitiveHeaders(customSensitiveHeaders);
+        sysGateway.setApplyEnable(applyEnable);
+        sysGateway.setStatus(status);
+        sysGateway.setDescription(description);
+
+        sysGatewayService.update(sysGateway, id);
+        return ResponseMsgUtil.success();
+    }
 }
